52d07b70129a180170d69feac4b39bc2461046f9
hvi <hvi@lesfurets.com>
2018-10-30 13:59:49 +0100

    Add custom hook example + useEffect example

--- src/react/hooks/StatefulFunctionalComponent.js
+++ src/react/hooks/StatefulFunctionalComponent.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
 /**
  * <> </> is a new shortcut for React.Fragment
@@ -15,6 +15,51 @@ const StatefulFunctionalComponent = () => {
   const onDecrement = () => setCounter(counter => counter - 1)
 
   return <>
+    Clicked: {counter} times
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </>
+}
+
+const StatefulFunctionalComponentRefactored = () => {
+  const [counter, onIncrement, onDecrement] = useCounter(0)
+
+  return <>
+    Clicked: {counter} times
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </>
+}
+
+/**
+ * Custom Hook example => reusable in multiple components
+ */
+const useCounter = (initialValue) => {
+  const [state, setCounter] = useState(initialValue)
+
+  const increment = () => setCounter(counter => counter + 1)
+  const decrement = () => setCounter(counter => counter - 1)
+  return [state, increment, decrement]
+}
+
+/**
+ * `useEffect` example :
+ * similar to `componentWillMount` `componentWillUnmount` and `componentWillUpdate` combined.
+ * It calls the function whenever a new render occurs.
+ * In this usage, we need to pass `[counter]`. Otherwise, the component renders, calls `incrementClicks`, which causes a
+ * new render, a new call to `incrementClicks`... and the component will infinitely keep rendering.
+ * `[counter]` passed as a second argument tells React to call the effect only if a change to an element of the passed
+ *  array occured. If previous counter `===` next counter then the effect won't trigger
+ */
+const StatefulFunctionalComponentSideEffect = () => {
+  const [counter, onIncrement, onDecrement] = useCounter(0)
+  const [clicks, incrementClicks] = useCounter(0)
+
+  useEffect(incrementClicks, [counter])
+
+
+  return <>
+    Rendered: {clicks} times
     Clicked: {counter} times
     <button onClick={onIncrement}>+</button>
     <button onClick={onDecrement}>-</button>
